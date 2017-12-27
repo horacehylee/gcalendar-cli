eb118c47b4a79f5985c3839e042b7441eacbbce7
Horace <horacehylee2@gmail.com>
2017-12-27 17:58:42 +0800

    remove source-map-support

--- backpack.config.js
+++ backpack.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   webpack: (config, options, webpack) => {
+    config.plugins.splice(1, 1) // remove the BannerPlugin
+    
     config.entry.main = [
       './src/index.ts'
     ]
